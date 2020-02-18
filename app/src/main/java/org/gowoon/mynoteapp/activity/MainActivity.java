7a64c9c199045ea42aa2e8b163d2143c15dafb0f
GowoonJ <jgw971229@naver.com>
2020-02-18 23:12:35 +0900

    package 추가

--- app/src/main/java/org/gowoon/mynoteapp/activity/MainActivity.java
+++ app/src/main/java/org/gowoon/mynoteapp/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package org.gowoon.mynoteapp;
+package org.gowoon.mynoteapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import org.gowoon.mynoteapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
