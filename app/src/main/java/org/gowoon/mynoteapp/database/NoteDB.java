5ad8744d2048f73e86482e337f1312f5a01aaa84
GowoonJ <jgw971229@naver.com>
2020-02-20 18:37:15 +0900

    Database 생성

---
+++ app/src/main/java/org/gowoon/mynoteapp/database/NoteDB.java
@@ -0,0 +1,32 @@
+package org.gowoon.mynoteapp.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {NoteTable.class, ImageTable.class}, version = 1, exportSchema = false)
+public abstract class NoteDB extends RoomDatabase {
+    public abstract NoteDao noteDao();
+
+    private static volatile NoteDB INSTANCE;
+    private static final int NUM_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExcutor = Executors.newFixedThreadPool(NUM_OF_THREADS);
+
+    static NoteDB getDatabase(final Context context){
+        if (INSTANCE == null){
+            synchronized (NoteDB.class){
+                if (INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext()
+                            ,NoteDB.class, "note_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
