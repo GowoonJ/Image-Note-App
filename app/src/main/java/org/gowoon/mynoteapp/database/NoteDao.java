ac753b9d7ca3f73d891f91f72c8b6bbc7920ca09
GowoonJ <jgw971229@naver.com>
2020-02-20 18:09:37 +0900

    NoteDao 생성 - room

---
+++ app/src/main/java/org/gowoon/mynoteapp/database/NoteDao.java
@@ -0,0 +1,37 @@
+package org.gowoon.mynoteapp.database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+    @Query("select * from note inner join image on note.id = image.note_id")
+    List<NoteTable> getAll();
+
+    @Query("select * from note inner join image on note.id = image.note_id where id = :id")
+    NoteTable getNoteDetail(int id);
+
+    @Query("select * from image")
+    List<ImageTable> getAllImg();
+
+    @Insert
+    void insertNote(NoteTable note);
+    @Insert
+    void insertImg(List<ImageTable> img);
+
+    @Update
+    void updateNote(NoteTable note);
+    @Update
+    void updateImg(List<ImageTable> img);
+
+    @Query("delete from note where id = :id")
+    void deleteNote(int id);
+
+    @Query("delete from image where note_id = :id")
+    void deleteNoteImg(int id);
+}
