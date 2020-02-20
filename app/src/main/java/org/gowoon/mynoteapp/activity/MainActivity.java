4082145faec66beedecf92cf0f88ad7da41ed212
GowoonJ <jgw971229@naver.com>
2020-02-20 20:37:11 +0900

    dataBinding <layout> 추가, recyclerView.setAdapter

--- app/src/main/java/org/gowoon/mynoteapp/activity/MainActivity.java
+++ app/src/main/java/org/gowoon/mynoteapp/activity/MainActivity.java
@@ -1,16 +1,27 @@
 package org.gowoon.mynoteapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import org.gowoon.mynoteapp.R;
+import org.gowoon.mynoteapp.adapter.NoteListAdapter;
+import org.gowoon.mynoteapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
+    public NoteListAdapter NoteListAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.recyclerNoteList.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL,false));
+        binding.recyclerNoteList.setAdapter(NoteListAdapter);
+
     }
 }
