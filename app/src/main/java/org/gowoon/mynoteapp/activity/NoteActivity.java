1edb02b74090ac56aafdeda6a9178c149e770548
GowoonJ <jgw971229@naver.com>
2020-02-22 02:09:23 +0900

    Note Activity 생성

---
+++ app/src/main/java/org/gowoon/mynoteapp/activity/NoteActivity.java
@@ -0,0 +1,16 @@
+package org.gowoon.mynoteapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.gowoon.mynoteapp.R;
+
+public class NoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+    }
+}
