4a31b4fc047aa4aba8650f65f9b372eecb1c7450
GowoonJ <jgw971229@naver.com>
2020-02-22 02:41:17 +0900

    binding 코드로 변경

--- app/src/main/java/org/gowoon/mynoteapp/activity/NoteActivity.java
+++ app/src/main/java/org/gowoon/mynoteapp/activity/NoteActivity.java
@@ -1,16 +1,26 @@
 package org.gowoon.mynoteapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
 import org.gowoon.mynoteapp.R;
+import org.gowoon.mynoteapp.databinding.LayoutNoteBinding;
 
 public class NoteActivity extends AppCompatActivity {
+
+    private LayoutNoteBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_note);
+        binding = DataBindingUtil.setContentView(this,R.layout.layout_note);
+
+        binding.buttonWrite.setOnClickListener(view -> getStringData());
+    }
+    private void getStringData(){
+
     }
 }
