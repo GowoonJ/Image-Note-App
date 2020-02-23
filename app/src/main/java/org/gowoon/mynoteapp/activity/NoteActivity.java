57c87541beb8b2303aab0e4b15d4b1cfc15d34ba
GowoonJ <jgw971229@naver.com>
2020-02-23 20:55:25 +0900

    BottomSheet buttonClickListener 수정

--- app/src/main/java/org/gowoon/mynoteapp/activity/NoteActivity.java
+++ app/src/main/java/org/gowoon/mynoteapp/activity/NoteActivity.java
@@ -1,30 +1,48 @@
 package org.gowoon.mynoteapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.LayoutInflaterCompat;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import org.gowoon.mynoteapp.R;
+import org.gowoon.mynoteapp.adapter.ImageListAdapter;
 import org.gowoon.mynoteapp.custom.CustomAppBar;
+import org.gowoon.mynoteapp.databinding.LayoutBottomSheetBinding;
 import org.gowoon.mynoteapp.databinding.LayoutNoteBinding;
 import org.gowoon.mynoteapp.fragment.BottomSheetDialog;
+import org.gowoon.mynoteapp.model.ImageData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gun0912.tedimagepicker.builder.TedImagePicker;
+import gun0912.tedimagepicker.builder.listener.OnMultiSelectedListener;
 
 public class NoteActivity extends AppCompatActivity {
 
+    ImageListAdapter imageListAdapter;
+    ArrayList<Uri> imageList = new ArrayList<>();
     int permissionCamera, permissionStorage;
     private static int MY_PERMISSIONS_REQUEST = 200;
     private LayoutNoteBinding binding;
     String title, content;
+
+    LayoutBottomSheetBinding bindingBottomSheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +56,8 @@ public class NoteActivity extends AppCompatActivity {
         binding.tvNoteImageAdd.setOnClickListener(view -> {
             getImage();
         });
+        binding.recyclerAddImage.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL,false));
+        binding.recyclerAddImage.setAdapter(imageListAdapter);
     }
     private void setCustomAppBar(){
         CustomAppBar customAppBar = new CustomAppBar(this, getSupportActionBar());
@@ -54,6 +74,17 @@ public class NoteActivity extends AppCompatActivity {
         if (permissionCamera == PackageManager.PERMISSION_GRANTED){
             BottomSheetDialog selectDialog = new BottomSheetDialog();
             selectDialog.show(getSupportFragmentManager(),"select");
+            selectDialog.setOnCamerraButtonClickListener(() -> {
+                Toast.makeText(this, "camera click", Toast.LENGTH_SHORT).show();
+                TedImagePicker.with(this)
+                        .startMultiImage(new OnMultiSelectedListener() {
+                            @Override
+                            public void onSelected(@NonNull List<? extends Uri> list) {
+                                imageList.addAll(list);
+                                imageListAdapter.setDataList(imageList);
+                            }
+                        });
+            });
         }else{
             Toast.makeText(this, "앱 사용권한을 확인해주세요", Toast.LENGTH_SHORT).show();
         }
--- app/src/main/java/org/gowoon/mynoteapp/fragment/BottomSheetDialog.java
+++ app/src/main/java/org/gowoon/mynoteapp/fragment/BottomSheetDialog.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,7 +16,14 @@ import org.gowoon.mynoteapp.R;
 import org.gowoon.mynoteapp.databinding.LayoutBottomSheetBinding;
 
 public class BottomSheetDialog extends BottomSheetDialogFragment {
-    LayoutBottomSheetBinding binding;
+    public LayoutBottomSheetBinding binding;
+    public Button btnCamera, btnUri;
+
+    public interface OnCameraButtonClickListener {
+        void onClick();
+    }
+    public OnCameraButtonClickListener cameraButtonClickListener = null;
+
 
     @Nullable
     @Override
@@ -23,7 +31,20 @@ public class BottomSheetDialog extends BottomSheetDialogFragment {
         binding = DataBindingUtil.inflate(inflater, R.layout.layout_bottom_sheet,container,false);
 
         binding.tvCancel.setOnClickListener(view -> dismiss());
+        binding.buttonCamera.setOnClickListener(view -> {
+           cameraButton();
+        });
 
         return binding.getRoot();
+    }
+
+    public void setOnCamerraButtonClickListener(OnCameraButtonClickListener listener){
+        this.cameraButtonClickListener = listener;
+    }
+    public void cameraButton(){
+        if(cameraButtonClickListener != null){
+            cameraButtonClickListener.onClick();
+        }
+        dismiss();
     }
 }
